4d43f3e36038fea8679daad6618c6c66e05753a9
Mohd Danish <danishm026@gmail.com>
2016-06-17 16:10:24 +0530

    Modified LoggingAspect to add pointcut for getModelBasePage method of CategoryPageParser.java

--- src/main/java/com/arc/jScraper/aspects/LoggingAspect.java
+++ src/main/java/com/arc/jScraper/aspects/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.arc.jScraper.aspects;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,9 +13,15 @@ import com.arc.jScraper.parsers.Parser;
 public class LoggingAspect {
 	private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 	
-	@AfterThrowing(pointcut="execution(public String com.arc.jScraper.parsers.HomePageParser.getCategoryLink(char))")
+	@AfterThrowing(pointcut="getCategoryLink() || getModelBasePage()")
 	public void logRetrievalFailure(JoinPoint joinPoint) {
 		Parser parser = (Parser) joinPoint.getTarget();
 		logger.error("Error Retrieving URL: {}", parser.getUrl());
 	}
+	
+	@Pointcut("execution(public String com.arc.jScraper.parsers.HomePageParser.getCategoryLink(char))")
+	public void getCategoryLink() {}
+	
+	@Pointcut("execution(public String com.arc.jScraper.parsers.CategoryPageParser.getModelBasePage(String))")
+	public void getModelBasePage() {}
 }
