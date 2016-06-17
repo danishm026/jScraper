90b0b0e204b0f43ccaf9e7c8f054e2cf6c0efcfd
Mohd Danish <danishm026@gmail.com>
2016-06-17 16:08:28 +0530

    Modified CategoryPageParserTest.java to run with SpringJUnit4ClassRunner and use injected parser

--- src/test/java/com/arc/jScraper/parsers/CategoryPageParserTest.java
+++ src/test/java/com/arc/jScraper/parsers/CategoryPageParserTest.java
@@ -5,12 +5,19 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:categoryPageParserTestConfig.xml")
 public class CategoryPageParserTest {
+	@Autowired
+	private CategoryPageParser parser;
+	
 	@Test
 	public void getModelBasePageTest() throws IOException {
-		CategoryPageParser parser = new CategoryPageParser();
-		
 		String categoryALink = "http://www.celebwallpaper.org/cat-a-1.htm";
 		parser.setUrl(categoryALink);
 		String modelName = "Aaliyah";
